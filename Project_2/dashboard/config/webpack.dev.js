dd0c07ad83f8c3a3f24967b0811dcd42731eef83
danielhdz23 <danielhdz.ingeniero@gmail.com>
2021-11-28 22:34:41 -0500

    created container deploy script

--- Project_2/dashboard/config/webpack.dev.js
+++ Project_2/dashboard/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -11,9 +12,14 @@ const devConfig = {
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
+    new ModuleFederationPlugin({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap'
+      },
+      shared: packageJson.dependencies,
+    })
   ],
 };
 
