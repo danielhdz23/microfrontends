25fd3e688786e3288262b9160efe11d2888918ef
danielhdz23 <danielhdz.ingeniero@gmail.com>
2021-12-03 17:48:37 -0500

    added workflow for marketing app

--- Project_2/marketing/src/components/Pricing.js
+++ Project_2/marketing/src/components/Pricing.js
@@ -15,9 +15,9 @@ import { Link as RouterLink } from 'react-router-dom';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color='inherit' href='https://material-ui.com/'>
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}
@@ -31,32 +31,32 @@ const useStyles = makeStyles((theme) => ({
     ul: {
       margin: 0,
       padding: 0,
-      listStyle: 'none',
-    },
+      listStyle: 'none'
+    }
   },
   toolbar: {
-    flexWrap: 'wrap',
+    flexWrap: 'wrap'
   },
   toolbarTitle: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   link: {
-    margin: theme.spacing(1, 1.5),
+    margin: theme.spacing(1, 1.5)
   },
   heroContent: {
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(8, 0, 6)
   },
   cardHeader: {
     backgroundColor:
       theme.palette.type === 'light'
         ? theme.palette.grey[200]
-        : theme.palette.grey[700],
+        : theme.palette.grey[700]
   },
   cardPricing: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'baseline',
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2)
   },
   footer: {
     borderTop: `1px solid ${theme.palette.divider}`,
@@ -65,9 +65,9 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(3),
     [theme.breakpoints.up('sm')]: {
       paddingTop: theme.spacing(6),
-      paddingBottom: theme.spacing(6),
-    },
-  },
+      paddingBottom: theme.spacing(6)
+    }
+  }
 }));
 
 const tiers = [
@@ -78,10 +78,10 @@ const tiers = [
       '10 users included',
       '2 GB of storage',
       'Help center access',
-      'Email support',
+      'Email support'
     ],
     buttonText: 'Sign up for free',
-    buttonVariant: 'outlined',
+    buttonVariant: 'outlined'
   },
   {
     title: 'Pro',
@@ -91,10 +91,10 @@ const tiers = [
       '20 users included',
       '10 GB of storage',
       'Help center access',
-      'Priority email support',
+      'Priority email support'
     ],
     buttonText: 'Get started',
-    buttonVariant: 'contained',
+    buttonVariant: 'contained'
   },
   {
     title: 'Enterprise',
@@ -103,16 +103,16 @@ const tiers = [
       '50 users included',
       '30 GB of storage',
       'Help center access',
-      'Phone & email support',
+      'Phone & email support'
     ],
     buttonText: 'Contact us',
-    buttonVariant: 'outlined',
-  },
+    buttonVariant: 'outlined'
+  }
 ];
 const footers = [
   {
     title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
+    description: ['Team', 'History', 'Contact us', 'Locations']
   },
   {
     title: 'Features',
@@ -121,8 +121,8 @@ const footers = [
       'Random feature',
       'Team feature',
       'Developer stuff',
-      'Another one',
-    ],
+      'Another one'
+    ]
   },
   {
     title: 'Resources',
@@ -130,13 +130,13 @@ const footers = [
       'Resource',
       'Resource name',
       'Another resource',
-      'Final resource',
-    ],
+      'Final resource'
+    ]
   },
   {
     title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-  },
+    description: ['Privacy policy', 'Terms of use']
+  }
 ];
 
 export default function Pricing() {
@@ -145,30 +145,28 @@ export default function Pricing() {
   return (
     <React.Fragment>
       {/* Hero unit */}
-      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+      <Container maxWidth='sm' component='main' className={classes.heroContent}>
         <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="textPrimary"
-          gutterBottom
-        >
+          component='h1'
+          variant='h2'
+          align='center'
+          color='textPrimary'
+          gutterBottom>
           Pricing
         </Typography>
         <Typography
-          variant="h5"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
+          variant='h5'
+          align='center'
+          color='textSecondary'
+          component='p'>
           Quickly build an effective pricing table for your potential customers
           with this layout. It&apos;s built with default Material-UI components
           with little customization.
         </Typography>
       </Container>
       {/* End hero unit */}
-      <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
+      <Container maxWidth='md' component='main'>
+        <Grid container spacing={5} alignItems='flex-end'>
           {tiers.map((tier) => (
             // Enterprise card is full width at sm breakpoint
             <Grid
@@ -176,8 +174,7 @@ export default function Pricing() {
               key={tier.title}
               xs={12}
               sm={tier.title === 'Enterprise' ? 12 : 6}
-              md={4}
-            >
+              md={4}>
               <Card>
                 <CardHeader
                   title={tier.title}
@@ -189,21 +186,20 @@ export default function Pricing() {
                 />
                 <CardContent>
                   <div className={classes.cardPricing}>
-                    <Typography component="h2" variant="h3" color="textPrimary">
+                    <Typography component='h2' variant='h3' color='textPrimary'>
                       ${tier.price}
                     </Typography>
-                    <Typography variant="h6" color="textSecondary">
+                    <Typography variant='h6' color='textSecondary'>
                       /mo
                     </Typography>
                   </div>
                   <ul>
                     {tier.description.map((line) => (
                       <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
-                      >
+                        component='li'
+                        variant='subtitle1'
+                        align='center'
+                        key={line}>
                         {line}
                       </Typography>
                     ))}
@@ -212,10 +208,9 @@ export default function Pricing() {
                 <CardActions>
                   <Button
                     component={RouterLink}
-                    to="/auth/signup"
+                    to='/auth/signup'
                     fullWidth
-                    color="primary"
-                  >
+                    color='primary'>
                     {tier.buttonText}
                   </Button>
                 </CardActions>
@@ -225,17 +220,17 @@ export default function Pricing() {
         </Grid>
       </Container>
       {/* Footer */}
-      <Container maxWidth="md" component="footer" className={classes.footer}>
-        <Grid container spacing={4} justifyContent="space-evenly">
+      <Container maxWidth='md' component='footer' className={classes.footer}>
+        <Grid container spacing={4} justifyContent='space-evenly'>
           {footers.map((footer) => (
             <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="textPrimary" gutterBottom>
+              <Typography variant='h6' color='textPrimary' gutterBottom>
                 {footer.title}
               </Typography>
               <ul>
                 {footer.description.map((item) => (
                   <li key={item}>
-                    <Link href="#" variant="subtitle1" color="textSecondary">
+                    <Link href='#' variant='subtitle1' color='textSecondary'>
                       {item}
                     </Link>
                   </li>
