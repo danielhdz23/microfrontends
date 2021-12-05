1f49e3f171821d3178b4f187e19fe9d965fc7f41
danielhdz23 <danielhdz.ingeniero@gmail.com>
2021-12-05 11:36:25 -0500

    added randomize css classes approach

--- Project_2/container/src/App.js
+++ Project_2/container/src/App.js
@@ -1,13 +1,18 @@
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
-
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'container'
+});
 export default () => {
   return (
     <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <Header />
         <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
