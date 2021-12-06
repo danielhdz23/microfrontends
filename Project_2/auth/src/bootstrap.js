8aefd1bfaac8aa66b7ed2a66a97521bd1273127e
danielhdz23 <danielhdz.ingeniero@gmail.com>
2021-12-05 19:08:57 -0500

    fixed app

--- Project_2/auth/src/bootstrap.js
+++ Project_2/auth/src/bootstrap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
-import App from './App';
+import App from './app';
 
 // Mount function to start up the app
 const mount = (el, { onNavigate, onSignIn, defaultHistory, initialPath }) => {
